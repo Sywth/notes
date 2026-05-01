f0db38c38f4f193853e7f06800783d6b59da612f
Sywth <114529087+Sywth@users.noreply.github.com>
2026-05-01 18:29:07 +0100

    feat: added bookmarks

--- notes/Dpll.lean
+++ notes/Dpll.lean
@@ -1,11 +1,3 @@
 #eval "DPLL in lean"
 
-theorem Example_3_2_4
-    (P Q R : Prop) (h : P → (Q → R)) : ¬R → (P → ¬Q) := by
-  assume h2 : ¬R
-  assume h3 : P
-  have h4 : Q → R := h h3
-  contrapos at h4       
-  show ¬Q from h4 h2
-  done
 
